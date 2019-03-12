e3ab0ffdbbad60627c5db9d62eaf1803b7054ca1
Mohamed Amn <mamn2@illinois.edu>
2019-03-12 16:36:55 -0500

    Added method comments in Sudoku.hpp

--- Sudoku/src/Sudoku.hpp
+++ Sudoku/src/Sudoku.hpp
@@ -20,28 +20,59 @@ public:
     std::string puzzle_s;
     std::array<std::array<char, 9>, 9> puzzle_a;
     
+    //Constructor for class, adds instance to sudoku_games vector
     explicit SudokuPuzzle();
+    
+    //Returns a string which is a "pretty" version of the Sudoku Puzzle
     std::string PrettyPrint() const;
+    
+    //Overloads operator for output, outputs to file if specified, or console if not
     friend std::ostream& operator<<(std::ostream& outStream, const SudokuPuzzle& sudoku);
+    
+    //Overloads the input operator, asks user for spf filepath
     friend std::istream& operator>>(std::istream& inStream, SudokuPuzzle& sudoku);
+
+    //Loads multiple puzzles from a filepath into SudokuPuzzle objects
+    //Returns true only if filepath is a valid spf file
     static bool LoadPuzzles(std::string& filepath);
+    
+    //Setter for puzzle_s, used only for file reading purposes
     void SetPuzzleS(const std::string setPuzzle_s);
+    
+    //Prints current state of the board
     void PrintToFile(std::string filepath) const;
     
+    //Attempts to solve the given puzzle, returns true if puzzle is successfully solved, false otherwise
     bool SolvePuzzle();
+    
+    //Searches for an empty tile in the puzzle, return true if empty tile is found
     bool FindEmptyTile(int &row, int &column) const;
+    
+    //Returns true if the current puzzle is possible (doesn't violate any rules of the game)
     bool PuzzleIsPossible(const int &row, const int &column, const int &tileNum) const;
+    
+    //Returns true if the number being added already exists in the row
     bool NumExistsInRow(const int &row, const int &checkNum) const;
+    
+    //Returns true if the number being added already exists in the column
     bool NumExistsInColumn(const int &column, const int &checkNum) const;
+    
+    //Returns true if the number being added already exists in the box
     bool NumExistsInBox(const int &boxRow, const int &boxColumn, const int &tileNum) const;
     
     
 private:
+    //Converts the string into an array, called when the string is set
     void inline SudokuStringToArray();
 };
 
+//Contains a list of all the sudoku games added from a file
 static std::vector<SudokuPuzzle*> sudoku_games;
+
+//Gets the sudoku game from the Sudoku.cpp file
 std::vector<SudokuPuzzle*>& GetSudokuGames();
+
+//Prints all the games added from a file;
 void PrintAllGames(const std::string& filepath);
 
 #endif /* Sudoko_hpp */
--- Sudoku/test/SudokuTest.cpp
+++ Sudoku/test/SudokuTest.cpp
@@ -11,52 +11,7 @@
 #include "catch.hpp"
 #include "Sudoku.hpp"
 
-TEST_CASE("Pretty Print") {
-
-    SECTION("Pretty print original unsolved game") {
-        SudokuPuzzle sudokuPuzzle;
-        std::string examplePuzzle = "___8_5____3__6___7_9___38___4795_3______71_9____2__5__1____248___9____5______6___";
-        sudokuPuzzle.SetPuzzleS(examplePuzzle);
-        std::string prettyPuzzle = "NEW BOARD\n\n0 0 0  8 0 5  0 0 0 \n0 3 0  0 6 0  0 0 7 \n0 9 0  0 0 3  8 0 0 \n\n0 4 7  9 5 0  3 0 0 \n0 0 0  0 7 1  0 9 0 \n0 0 0  2 0 0  5 0 0 \n\n1 0 0  0 0 2  4 8 0 \n0 0 9  0 0 0  0 5 0 \n0 0 0  0 0 6  0 0 0 \n\n";
-        REQUIRE(prettyPuzzle == sudokuPuzzle.PrettyPrint());
-    }
-    
-    SECTION("Pretty print solution") {
-        
-        SudokuPuzzle sudokuPuzzle;
-        std::string examplePuzzle = "_7___1___6____________53______8___2__3__4716_4_____57_____1_75__6_52__4_3____9_86";
-        sudokuPuzzle.SetPuzzleS(examplePuzzle);
-        sudokuPuzzle.SolvePuzzle();
-        std::string solvedPuzzle = "NEW BOARD\n\n5 7 9  6 8 1  4 3 2 \n6 2 3  7 9 4  8 1 5 \n1 8 4  2 5 3  6 9 7 \n\n7 1 6  8 3 5  9 2 4 \n2 3 5  9 4 7  1 6 8 \n4 9 8  1 6 2  5 7 3 \n\n8 4 2  3 1 6  7 5 9 \n9 6 7  5 2 8  3 4 1 \n3 5 1  4 7 9  2 8 6 \n\n";
-        CHECK(solvedPuzzle == sudokuPuzzle.PrettyPrint());
-        
-    }
-
-    SECTION("Pretty print solution to file") {
-        
-        SudokuPuzzle sudokuPuzzle;
-        std::string examplePuzzle = "_7___1___6____________53______8___2__3__4716_4_____57_____1_75__6_52__4_3____9_86";
-        sudokuPuzzle.SetPuzzleS(examplePuzzle);
-        sudokuPuzzle.SolvePuzzle();
-        std::string filepath = "/Users/mohamedamn/Documents/sudoku-mamn2/Sudoku/Sudoku/Solutions";
-        sudokuPuzzle.PrintToFile(filepath);
-        
-        std::ifstream myFile;
-        std::string line;
-        myFile.open(filepath);
-        
-        //goes to line 3 to check that line
-        for (int i = 1; i <= 3; i++) {
-            getline(myFile, line);
-        }
-        
-        CHECK(line == "5 7 9  6 8 1  4 3 2 ");
-        
-    }
-
-}
-
-TEST_CASE("Input Stream") {
+TEST_CASE("File Input") {
 
     SECTION("Empty File") {
         std::string fileName("/Users/mohamedamn/Documents/sudoku-mamn2/Sudoku/Sudoku/EmptyFile");
@@ -98,6 +53,18 @@ TEST_CASE("Input Stream") {
         }
     }
 
+}
+
+TEST_CASE("Vertical Check") {
+    
+}
+
+TEST_CASE("Box Check") {
+    
+}
+
+TEST_CASE("Horizontal Check") {
+    
 }
 
 TEST_CASE("SolvePuzzle") {
@@ -153,10 +120,47 @@ TEST_CASE("SolvePuzzle") {
 
 }
 
-TEST_CASE("Vertical Check") {
-
-}
-
-TEST_CASE("Box Check") {
-
+TEST_CASE("Pretty Print") {
+    
+    SECTION("Pretty print original unsolved game") {
+        SudokuPuzzle sudokuPuzzle;
+        std::string examplePuzzle = "___8_5____3__6___7_9___38___4795_3______71_9____2__5__1____248___9____5______6___";
+        sudokuPuzzle.SetPuzzleS(examplePuzzle);
+        std::string prettyPuzzle = "NEW BOARD\n\n0 0 0  8 0 5  0 0 0 \n0 3 0  0 6 0  0 0 7 \n0 9 0  0 0 3  8 0 0 \n\n0 4 7  9 5 0  3 0 0 \n0 0 0  0 7 1  0 9 0 \n0 0 0  2 0 0  5 0 0 \n\n1 0 0  0 0 2  4 8 0 \n0 0 9  0 0 0  0 5 0 \n0 0 0  0 0 6  0 0 0 \n\n";
+        REQUIRE(prettyPuzzle == sudokuPuzzle.PrettyPrint());
+    }
+    
+    SECTION("Pretty print solution") {
+        
+        SudokuPuzzle sudokuPuzzle;
+        std::string examplePuzzle = "_7___1___6____________53______8___2__3__4716_4_____57_____1_75__6_52__4_3____9_86";
+        sudokuPuzzle.SetPuzzleS(examplePuzzle);
+        sudokuPuzzle.SolvePuzzle();
+        std::string solvedPuzzle = "NEW BOARD\n\n5 7 9  6 8 1  4 3 2 \n6 2 3  7 9 4  8 1 5 \n1 8 4  2 5 3  6 9 7 \n\n7 1 6  8 3 5  9 2 4 \n2 3 5  9 4 7  1 6 8 \n4 9 8  1 6 2  5 7 3 \n\n8 4 2  3 1 6  7 5 9 \n9 6 7  5 2 8  3 4 1 \n3 5 1  4 7 9  2 8 6 \n\n";
+        CHECK(solvedPuzzle == sudokuPuzzle.PrettyPrint());
+        
+    }
+    
+    SECTION("Pretty print solution to file") {
+        
+        SudokuPuzzle sudokuPuzzle;
+        std::string examplePuzzle = "_7___1___6____________53______8___2__3__4716_4_____57_____1_75__6_52__4_3____9_86";
+        sudokuPuzzle.SetPuzzleS(examplePuzzle);
+        sudokuPuzzle.SolvePuzzle();
+        std::string filepath = "/Users/mohamedamn/Documents/sudoku-mamn2/Sudoku/Sudoku/Solutions";
+        sudokuPuzzle.PrintToFile(filepath);
+        
+        std::ifstream myFile;
+        std::string line;
+        myFile.open(filepath);
+        
+        //goes to line 3 to check that line
+        for (int i = 1; i <= 3; i++) {
+            getline(myFile, line);
+        }
+        
+        CHECK(line == "5 7 9  6 8 1  4 3 2 ");
+        
+    }
+    
 }
